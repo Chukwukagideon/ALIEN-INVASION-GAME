a39b72d7bf37ad6d81b622f678c4b1bc34b3a6f2
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-25 18:01:09 +0100

    Imported the settings module and updated the code to work with the imported settings module

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,14 +4,13 @@ import pygame
 
 from settings import Settings
 
-
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.settings = settings.Settings()
+        self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
