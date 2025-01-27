e4f65a611e37bff252e6529ec076b758364b9556
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-27 23:44:06 +0100

    refactored the code and added 2 helper methods _check_events() to manage events and _update_screen() to manage the code responsible for updating rhe screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,29 +13,34 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
-
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse event
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        # Watch for keyboard and mouse event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # make the most recently drawn screen visible
-            pygame.display.flip()
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # make the most recently drawn screen visible
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
     # make a game instance and run the game
     ai = AlienInvasion()
     ai.run_game()
+
+
