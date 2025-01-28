bdfb4bcd9a3e5148cff56974c7fa3e8150b3fb22
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-28 23:03:54 +0100

    refactored code, by breaking down the _check_events method into 2 methods _check_key_down_events & _check_key_up_events

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,22 +25,30 @@ class AlienInvasion:
             self._update_screen()
 
     def _check_events(self):
-        # Watch for keyboard and mouse event
+        """Watch for keyboard and mouse event"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # Move the ship to the left
-                    self.ship.moving_left = True
+                self._check_key_down_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_key_up_events(event)
+
+    def _check_key_down_events(self, event):
+        """Respond to key presses"""
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move the ship to the left
+            self.ship.moving_left = True
+
+    def _check_key_up_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop
