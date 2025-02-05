0c3ddf67477bf8909ce061073eeb7c98431cdb93
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-02-05 16:17:59 +0100

    added the _update_bullets method to handle updating bullets position on screen and deleting the ones that has gone off screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,14 +30,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update_position()
             self.bullets.update()
-
-            # Get rid of the bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
             
     def _check_events(self):
@@ -72,8 +65,15 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        # Get rid of the bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop
