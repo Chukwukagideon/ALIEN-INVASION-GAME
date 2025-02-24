ed6078bda75227a5d8401bbf3b47e6c653bf4398
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-02-24 01:35:58 +0100

    update: Game over

--- game_stats.py
+++ game_stats.py
@@ -3,6 +3,9 @@ class GameStats:
 
     def __init__(self, ai_game):
         """Initialize statistics"""
+        # start Alien invasion in an active state
+        self.game_active = True
+
         self.settings = ai_game.settings
         self.restart_stats()
 
